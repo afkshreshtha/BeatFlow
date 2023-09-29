d8549a6730fc7d032405ca8c911a767a5af52ee8
afkshreshtha <shreshtha1345@gmail.com>
2023-09-29 16:28:35 +0530

    import useeffect

--- app/likedsongs/components/SongCard.jsx
+++ app/likedsongs/components/SongCard.jsx
@@ -1,4 +1,5 @@
-import React,{useEffect} from 'react'
+"use client"
+import React,{useEffect, useState} from 'react'
 import { useRouter } from 'next/navigation'
 import { useDispatch } from 'react-redux'
 
