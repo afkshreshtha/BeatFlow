c3bc5272352449512cd72fda96d6af7fedc81794
afkshreshtha <shreshtha1345@gmail.com>
2023-10-01 11:24:49 +0530

    metadata

--- app/layout.js
+++ app/layout.js
@@ -4,51 +4,53 @@ import Sidebar from "./components/Sidebar";
 import Searchbar from "./components/Searchbar";
 import "./globals.css";
 import { Providers } from "./redux/provider";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 export const metadata = {
   title: "TuneWave",
-  
 };
 export default function RootLayout({ children }) {
-  
-
   return (
+    <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="QibisYWcAxZwed8-_w-2OVseaL58KAxSe7Jb2A6wXLQ"
+        />
+      </head>
 
-      <html lang="en">
-        <body>
-          <div className="relative flex">
-            {/* Sidebar */}
-            <div className="fixed top-0 left-0 h-full">
-              <Sidebar />
-            </div>
+      <body>
+        <div className="relative flex">
+          {/* Sidebar */}
+          <div className="fixed top-0 left-0 h-full">
+            <Sidebar />
+          </div>
 
-            {/* Main Content */}
-            <div className="flex-1 flex flex-col md:pl-64">
-              {" "}
-              {/* Add padding-left to avoid content overlap */}
-              <Searchbar />
-              <div className="flex-1 pb-40 px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-                <div className="flex-1 h-fit pb-40">
-                  <Providers>
-                    {children}
-                    <div className="fixed bottom-0 left-0 w-full h-30  bg-gray-500">
-                      <Link href="songs/"></Link>
-                      <MusicPlayer />
-                      <div className="md:hidden">
-                        <MobileNav />
-                      </div>
+          {/* Main Content */}
+          <div className="flex-1 flex flex-col md:pl-64">
+            {" "}
+            {/* Add padding-left to avoid content overlap */}
+            <Searchbar />
+            <div className="flex-1 pb-40 px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+              <div className="flex-1 h-fit pb-40">
+                <Providers>
+                  {children}
+                  <div className="fixed bottom-0 left-0 w-full h-30  bg-gray-500">
+                    <Link href="songs/"></Link>
+                    <MusicPlayer />
+                    <div className="md:hidden">
+                      <MobileNav />
                     </div>
-                    <ToastContainer/>
-                  </Providers>
-                </div>
+                  </div>
+                  <ToastContainer />
+                </Providers>
               </div>
             </div>
           </div>
-        </body>
-      </html>
-
+        </div>
+      </body>
+    </html>
   );
 }
